801817f0ee0926d433640a3af8a3de485bda5fea
mohamedsrour0 <mohammedsrour2004@gmail.com>
2026-09-20 22:49:14 +0300

    send initial version without Ai

---
+++ CheckoutFacade.java
@@ -0,0 +1,55 @@
+package lab2;
+
+ class InventoryService{
+   
+    public  boolean isAvailable(String item,int qty){
+        System.out.println(item +"is available");
+        return true;
+    }
+}
+
+ class PaymentService{
+    public boolean charge(String account,double amount){
+        System.out.println("account is valide");
+        return true;
+    }
+
+}
+
+class ShippingService{
+    public boolean schedule(String  item , String adress){
+        System.out.println(item +"we be shipped to"+adress);
+        return true;
+    }
+}
+
+class NotificationService{
+    public boolean sendConfirmation(String email){
+        System.out.println("sending confirmation to "+email);
+        return  true;
+    }
+}
+
+
+
+
+public class CheckoutFacade {
+    InventoryService inventory=new InventoryService();
+     PaymentService pay= new PaymentService();
+     ShippingService ship= new ShippingService();
+     NotificationService not = new NotificationService();
+
+    public boolean placeOrder(String item, int qty, String account,
+        double amount,String adress,String email){
+            if (!inventory.isAvailable(item, qty)){
+                return false;
+            }
+            if (!pay.charge(account, amount)){
+                return  false;
+            }
+            ship.schedule(item, adress);
+            not.sendConfirmation(email);
+            return  true;
+        }
+    
+}
